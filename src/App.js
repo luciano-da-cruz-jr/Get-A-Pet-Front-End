913b71ac8cd1e310ee812fc0351f331b82fb494c
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-09-20 18:48:40 -0300

    Criação do component CONTAINER 268 V1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Footer from './components/layout/Footer';
-import Navbar from './components/layout/Navbar';
+
+/* components */
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import Container from './components/layout/Container'
 
 /* pages */
-
 import Login from './components/pages/Auth/Login'
 import Register from './components/pages/Auth/Register'
 import Home from './components/pages/Home'
@@ -13,11 +15,13 @@ function App() {
 
     <Router>
       <Navbar/>
-      <Routes>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/" element={<Home/>} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </Container>
       <Footer/>
     </Router>
   );
