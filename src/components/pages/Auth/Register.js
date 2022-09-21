df1fc4f3dcd0d7ddd9731311d690cebd06a7f333
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-09-20 21:52:46 -0300

    conexao com API implementado 274 V1

--- src/components/pages/Auth/Register.js
+++ src/components/pages/Auth/Register.js
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 
 import Input from "../../form/Input"
 import { Link } from 'react-router-dom'
 
 import styles from '../../form/Form.module.css'
 
+/* contexts */
+import { Context } from '../../../context/UserContext'
+
 function Register() {
     const [user, setUser] = useState({})
+    const { register } = useContext(Context)
 
     function handleChange(e) {
         setUser({ ...user, [e.target.name]: e.target.value })
@@ -15,6 +19,7 @@ function Register() {
     function handleSubmit(e){
         e.preventDefault()
         // enviar o usuário para o banco
+        register(user)
     }
 
     return (
