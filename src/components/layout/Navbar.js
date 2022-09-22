1569212743c6a8af9a0d1a09b243128abaf55efa
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-09-22 13:40:11 -0300

    start profile page implementado 283 V1

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -24,8 +24,16 @@ function Navbar() {
                         <Link to="/">Adotar</Link>
                     </>
                 </li>
-                {authenticated ? (<li onClick={logout}>Sair</li>) : (
+                {authenticated ? (
+                <>
+                    <li>
+                        <Link to="/user/profile">Perfil</Link>
+                    </li>
+                    <li onClick={logout}>Sair</li>
+                </>
+                ) : (
                     <>
+                        
                         <li>
                             <Link to="/login">Entrar</Link>
                         </li>
