493e1dae7b5eb1618e94fa0e94427046b971342f
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-09-22 19:29:33 -0300

    list pets page implementado 290 V1

--- src/App.js
+++ src/App.js
@@ -11,9 +11,11 @@ import Login from './components/pages/Auth/Login'
 import Register from './components/pages/Auth/Register'
 import Home from './components/pages/Home'
 import Profile from './components/pages/User/Profile'
+import MyPets from './components/pages/Pet/MyPets'
 
 /* context */
 import {UserProvider} from './context/UserContext'
+
 
 function App() {
   return (
@@ -27,6 +29,7 @@ function App() {
             <Route path="/login" element={<Login/>} />
             <Route path="/register" element={<Register/>} />
             <Route path="/user/profile" element={<Profile/>} />
+            <Route path="/pet/mypets" element={<MyPets/>} />
             <Route path="/" element={<Home/>} />
           </Routes>
         </Container>
