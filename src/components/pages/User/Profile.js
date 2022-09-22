4b2cd8d2ad9fb58bb278f3bc72d262a85cc64487
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-09-22 15:38:32 -0300

    update usuario sem img implementado 286 V1

--- src/components/pages/User/Profile.js
+++ src/components/pages/User/Profile.js
@@ -1,3 +1,5 @@
+import api from '../../../utils/api'
+
 import Input from '../../form/Input'
 
 import { useState, useEffect } from 'react'
@@ -5,15 +7,60 @@ import { useState, useEffect } from 'react'
 import styles from './Profile.module.css'
 import formStyles from '../../form/Form.module.css'
 
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
 function Profile() {
     const [user, setUser] = useState({})
+    const [token] = useState(localStorage.getItem('token') || '')
+    const { setFlashMessage } = useFlashMessage()
+
+    useEffect(() => {
+        api
+          .get('/users/checkuser', {
+            headers: {
+              Authorization: `Bearer ${JSON.parse(token)}`,
+            },
+          })
+          .then((response) => {
+            setUser(response.data)
+          })
+      }, [token])
 
     function handleChange(e) {
-
-      }
+        setUser({ ...user, [e.target.name]: e.target.value })
+    }
     
-      function onFileChange(e) {
-        
+    function onFileChange(e) {
+        setUser({ ...user, [e.target.name]: e.target.files[0] })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+    
+        let msgType = 'success'
+    
+        const formData = new FormData()
+    
+        await Object.keys(user).forEach((key) => formData.append(key, user[key]),)
+    
+        const data = await api
+          .patch(`/users/edit/${user._id}`, formData, {
+            headers: {
+              Authorization: `Bearer ${JSON.parse(token)}`,
+              'Content-Type': 'multipart/form-data',
+            },
+          })
+          .then((response) => {
+            console.log(response.data)
+            return response.data
+          })
+          .catch((err) => {
+            console.log(err)
+            msgType = 'error'
+            return err.response.data
+          })
+    
+        setFlashMessage(data.message, msgType)
       }
 
     return (
@@ -22,7 +69,7 @@ function Profile() {
                 <h1>Perfil</h1>
                 <p>Preview Image</p>
             </div>
-            <form className={formStyles.form_container}>
+            <form onSubmit={handleSubmit} className={formStyles.form_container}>
             <Input
                 text="Imagem"
                 type="file"
