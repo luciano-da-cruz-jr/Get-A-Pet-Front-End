14ccf490f1f43cb99a54745e311176f33671b852
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-09-22 11:39:40 -0300

    register, login e logout back e front implementado 282 V1

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -48,6 +48,27 @@ export default function useAuth(){
         navigate('/')
     }
 
+    async function login(user) {
+
+        const msgText = 'Login realizado com sucesso!'
+        const msgType = 'success'
+
+        try {
+
+            const data = await api.post('/users/login', user).then((response) => {
+                return response.data
+            })
+
+            await authUser(data)
+            
+        } catch (error) {
+            msgText = error.response.data.message
+            msgType = 'error'
+        }
+
+        setFlashMessage(msgText, msgType)
+    }
+
     function logout() {
         const msgText = 'Logout realizado com sucesso!'
         const msgType = 'success'
@@ -58,6 +79,6 @@ export default function useAuth(){
         navigate('/')
     }
 
-    return { authenticated, register, logout }
+    return { authenticated, register, logout, login }
 
 }
