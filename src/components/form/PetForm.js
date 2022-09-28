c37d8aeb6a00f3548f87767f1eb348a35b236123
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-09-28 15:18:14 -0300

    Form Pet iniciado 291 V1

---
+++ src/components/form/PetForm.js
@@ -0,0 +1,62 @@
+import {useState} from 'react'
+
+import formStyles from './Form.module.css'
+
+import Input from './Input'
+import Select from './Select'
+
+function PetForm({handleSubmit, petData, btnText}) {
+    const [pet, setPet] = useState(petData || {})
+    const [preview, setPreview] = useState([])
+    const colors = ['Branco', 'Preto', 'Cinza', 'Caramelo', 'Mesclado']
+
+    function onFileChange(e) { }
+    function handleOnChange(e) { }
+    function handleColor(e) { }
+
+    return (
+        <form className={formStyles.form_container}>
+            <Input
+                text="Imagens do Pet"
+                type="file"
+                name="images"
+                handleOnChange={onFileChange}
+                multiple={true}
+            />
+             <Input
+                text="Nome do Pet"
+                type="text"
+                name="name"
+                placeholder="Digite o nome"
+                handleOnChange={handleOnChange}
+                value={pet.name || ''}
+            />
+            <Input
+                text="Idade do Pet"
+                type="text"
+                name="age"
+                placeholder="Digite a idade"
+                handleOnChange={handleOnChange}
+                value={pet.age || ''}
+            />
+            <Input
+                text="Peso do Pet"
+                type="number"
+                name="weight"
+                placeholder="Digite o peso"
+                handleOnChange={handleOnChange}
+                value={pet.weight || ''}
+            />
+            <Select
+                name="color"
+                text="Selecione a cor"
+                options={colors}
+                handleOnChange={handleColor} 
+                value={pet.color || ''}
+            />
+            <input type="submit" value={btnText} />
+        </form>
+    )
+}
+
+export default PetForm
