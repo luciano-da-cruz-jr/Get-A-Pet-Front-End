7d56420a7ac69b12b0f34cdb21a57e2e90e8c78f
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-10-11 10:18:51 -0300

    remove pet implementado 297 V1

--- src/components/pages/Pet/MyPets.js
+++ src/components/pages/Pet/MyPets.js
@@ -27,6 +27,29 @@ function MyPets() {
           })
       }, [token])
 
+    async function removePet(id) {
+    let msgType = 'success'
+    
+    const data = await api
+        .delete(`/pets/${id}`, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`,
+            },
+        })
+        .then((response) => {
+            const updatedPets = pets.filter((pet) => pet._id != id)
+            setPets(updatedPets)
+            return response.data
+        })
+        .catch((err) => {
+            console.log(err)
+            msgType = 'error'
+            return err.response.data
+        })
+    
+        setFlashMessage(data.message, msgType)
+    }
+
     return (
         <section>
             <div className={styles.petslist_header}>
@@ -52,7 +75,13 @@ function MyPets() {
                                         </button>
                                     )}
                                     <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
-                                    <button>Excluir</button>
+                                    <button
+                                        onClick={() => {
+                                            removePet(pet._id)
+                                        }}
+                                    >
+                                        Excluir
+                                    </button>
                                 </>) 
                                 : <p>Pet já adotado</p>}
                             </div>
