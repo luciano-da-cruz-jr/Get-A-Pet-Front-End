eff7874011a6a2d364432eed6769eb98fc8f58fe
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-10-11 09:46:07 -0300

    dashboard pets implementado 295 V2

--- src/components/pages/Pet/MyPets.js
+++ src/components/pages/Pet/MyPets.js
@@ -1,6 +1,8 @@
 import api from '../../../utils/api'
 import {useState, useEffect} from 'react'
 
+import styles from './Dashboard.module.css'
+
 import { Link } from 'react-router-dom'
 
 import RoundedImage from '../../layout/RoundedImage'
@@ -32,7 +34,28 @@ function MyPets() {
                 <Link to="/pet/add">Cadastrar Pet</Link>
             </div>
             <div>
-                {pets.length > 0 && <p>Meus Pets cadastrados</p>}
+                {pets.length > 0 &&
+                    pets.map((pet) => (
+                        <div key={pet._id}>
+                            <RoundedImage
+                                src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
+                                alt={pet.name}
+                                width="px75"
+                            />
+                            <span className="bold">{pet.name}</span>
+                            <div className={styles.actions}>
+                                {pet.available ? 
+                                (<>
+                                    {pet.adopter && (
+                                        <button>Concluir adoção</button>
+                                    )}
+                                    <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
+                                    <button>Excluir</button>
+                                </>) 
+                                : <p>Pet já adotado</p>}
+                            </div>
+                        </div>
+                ))}
                 {pets.length === 0 && <p>Não há pets cadastrados</p>}
             </div>
         </section>
