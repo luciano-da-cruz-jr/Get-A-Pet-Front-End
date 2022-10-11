b91be62febea8bac6e0f4b8d49467cde4d36e913
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-10-11 16:21:44 -0300

    page edit pet implementado 300 V3

--- src/components/pages/Pet/EditPet.js
+++ src/components/pages/Pet/EditPet.js
@@ -29,7 +29,41 @@ function EditPet() {
       })
   }, [token, id])
 
-  async function updatePet(pet) {}
+  async function updatePet(pet) {
+    let msgType = 'success'
+
+    const formData = new FormData()
+
+    await Object.keys(pet).forEach((key) => {
+      if (key === 'images') {
+        for (let i = 0; i < pet[key].length; i++) {
+          formData.append(`images`, pet[key][i])
+        }
+      } else {
+        formData.append(key, pet[key])
+      }
+    })
+
+    const data = await api
+      .patch(`pets/${pet._id}`, formData, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((response) => {
+        console.log(response.data)
+        return response.data
+      })
+      .catch((err) => {
+        console.log(err)
+        msgType = 'error'
+        return err.response.data
+      })
+
+    setFlashMessage(data.message, msgType)
+
+  }
 
   return (
     <section>
