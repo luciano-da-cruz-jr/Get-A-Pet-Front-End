2a6776ba419c346dfe322bd5feff47405e195860
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-10-12 08:14:56 -0300

    findall pet na home implementado 302 V2

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -19,7 +19,31 @@ function Home() {
 
     return (
         <section>
-            <h1>Home</h1>
+            <div className={styles.pet_home_header}>
+                <h1>Adote um Pet</h1>
+                <p>Veja os detalhes de cada um e conheça o tutor deles</p>
+            </div>
+            <div className={styles.pet_container}>
+                {pets.length > 0 && 
+                    pets.map((pet) => (
+                        <div>
+                            <p>Imagm do Pet</p>
+                            <h3>{pet.name}</h3>
+                            <p>
+                                <span className="bold">Peso:</span> {pet.weight}kg
+                            </p>
+                            {pet.available ? (
+                                <Link to={`/pet/${pet._id}`}>Mais detalhes</Link>
+                            ) : (
+                                <p className={styles.adopted_text}>Adotado!</p>
+                            )}
+                        </div>
+                    ))
+                }
+                {pets.length === 0 && (
+                    <p>Não há pets cadastrados ou disponíveis para adoção no momento!</p>
+                )}
+            </div>
         </section>
     )
 }
