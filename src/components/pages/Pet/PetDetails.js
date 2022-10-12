15f31be4e1049aa850e660a34beb64609464df82
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-10-12 08:41:20 -0300

    page detalhes pet implementado 304 V1

---
+++ src/components/pages/Pet/PetDetails.js
@@ -0,0 +1,18 @@
+import api from '../../../utils/api'
+
+import { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+
+import styles from './PetDetails.module.css'
+
+/* hooks */
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
+function PetDetails() {
+  return (
+    <h1>Página de Pet</h1>
+  )
+  
+}
+
+export default PetDetails
